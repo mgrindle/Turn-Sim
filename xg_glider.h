1eb02742bbe02b32818189b320aa707752206211
ty-z973 <mgca208@summitxo.com>
2017-03-27 12:45:13 -0600

    Begin defining XG_Glider class

--- xg_glider.h
+++ xg_glider.h
@@ -18,4 +18,32 @@
 // End constant definitions
 
 
+//****************************************
+// for class type: XG_Vario_Press
+//****************************************
+class XG_Vario_Press {
+private:
+
+public:
+
+};
+
+
+//****************************************
+// for class type: XG_Glider
+//****************************************
+class XG_Glider {
+private:
+    AP_Point _g_loc;                // current location
+    AP_Speed_Vec _g_heading_vec;    // current heading vector (dir, as)
+    int _g_curr_vert_sp;            // current vertical speed (cm/ds)
+    char _g_curr_sp_mode;           // current speed mode (c,s,t)
+public:
+
+};
+
+
+
+
+
 #endif // XG_GLIDER_H_INCLUDED
